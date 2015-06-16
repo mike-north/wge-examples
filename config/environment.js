216eed5d8119c754595d3eb8c34ebef03e9d1644
Mike North <michael.l.north@gmail.com>
2015-06-16 07:49:01 -0400

    only use wge-examples baseURL in prod

--- config/environment.js
+++ config/environment.js
@@ -4,8 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'wge-examples',
     environment: environment,
-    baseURL: '/wge-examples',
-    locationType: 'hash',
+    baseURL: '/',
+    locationType: 'auto',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -40,7 +40,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.baseURL = '/wge-examples';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
