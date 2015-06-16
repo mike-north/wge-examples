fd4bce76485c0cc76d43792aff36287da53b0388
Mike North <michael.l.north@gmail.com>
2015-06-16 02:26:20 -0400

    gh pages

--- config/environment.js
+++ config/environment.js
@@ -4,8 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'wge-examples',
     environment: environment,
-    baseURL: '/',
-    locationType: 'auto',
+    baseURL: '/wge-examples',
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
