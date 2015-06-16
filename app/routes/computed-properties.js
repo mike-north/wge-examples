1db1b5c85ddd9f836143ef9c0c30f4f2aa3a9591
Mike North <michael.l.north@gmail.com>
2015-06-16 14:28:46 -0400

    mock API responses in prod

--- app/routes/computed-properties.js
+++ app/routes/computed-properties.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
+import Pretender from 'pretender';
+import RestaurantMenu from 'wge-examples/tests/page-objects/restaurant-menu';
+
 export default Ember.Route.extend({
+  init() {
+    this._super(...arguments);
+    let server = new Pretender();
+    this.set('server', server);
+    new RestaurantMenu().prepareRequests(server);
+  },
+
+  destroy() {
+    this.get('server').shutdown();
+
+  }
 });
